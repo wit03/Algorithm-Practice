e3985631f487d7ebacb0b716003c1cead986390f
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2020-01-21 09:53:34 +0700

    :tada: init /kmutt/week-1

---
+++ kmutt/week-1/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+
+    mainควร return อะไรสักอย่างให้ OS ไม่ควรเป็น void (ไม่returnอะไร)
+
+    \t = tabs
+    \r = new line (window)
+    \l = new line (UNIX)
+*/
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;
+}
